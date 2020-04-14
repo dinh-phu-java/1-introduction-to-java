d572f861717409dd8f6ba7c646efe33f2190a344
Dinh Phu <ndp17hp@gmail.com>
2020-04-14 08:34:14 +0700

    them file bai tập

---
+++ Thuc hanh Hien thi thoi gian he thong/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf(" %s %2$tB %2$td %2$tY Hour: %2$tR","Due Date",new Date());
+
+    }
+}
